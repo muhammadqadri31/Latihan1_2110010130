7b993c50aa9efa1772d390dcf7327659ba916430
muhammadqadri31 <muhammadqadri31@gmail.com>
2023-06-12 18:36:37 +0800

    Membuat class MatematikaBeraksi

--- src/hitung/MatematikaBeraksi.java
+++ src/hitung/MatematikaBeraksi.java
@@ -1,5 +1,13 @@
 package hitung;
 
 public class MatematikaBeraksi {
-    
+    public static void main(String[] args) {
+        //membuat objek
+        Matematika qadri = new Matematika(3,0);
+        
+        System.out.println("Hasil penjumlahan: "+qadri.setPenjumlahan());
+        System.out.println("Hasil pengurangan: "+qadri.setPengurangan());
+        System.out.println("Hasil perkalian: "+qadri.setPerkalian());
+        System.out.println("Hasil pembagian: "+qadri.setPembagian());
+    }
 }
